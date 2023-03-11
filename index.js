a8f21e7e9ea8c807efb0caf038c167bfcb1d9648
uselesss <dushebaa75@gmail.com>
2023-03-11 20:42:44 +0300

    Remove cors import from index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const sequelize = require('./db');
 const { Sayings } = require('./models/models');
 
